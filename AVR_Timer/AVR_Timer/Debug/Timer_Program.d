7e8ef7aa24887f3ff657f439f268faea75dca166
MoHassanAli <86575412+MoHassanAli@users.noreply.github.com>
2021-07-11 00:13:29 +0200

    Add files via upload

---
+++ AVR_Timer/AVR_Timer/Debug/Timer_Program.d
@@ -0,0 +1,34 @@
+Timer_Program.d Timer_Program.o: .././Timer_Program.c .././std_type.h \
+ .././bit_math.h .././DIO_Interface.h .././Timer_Private.h \
+ .././Timer_Confige.h \
+ c:\program\ files\ (x86)\atmel\studio\7.0\toolchain\avr8\avr8-gnu-toolchain\avr\include\avr\delay.h \
+ c:\program\ files\ (x86)\atmel\studio\7.0\toolchain\avr8\avr8-gnu-toolchain\avr\include\util\delay.h \
+ c:\program\ files\ (x86)\atmel\studio\7.0\toolchain\avr8\avr8-gnu-toolchain\avr\include\inttypes.h \
+ c:\program\ files\ (x86)\atmel\studio\7.0\toolchain\avr8\avr8-gnu-toolchain\lib\gcc\avr\5.4.0\include\stdint.h \
+ c:\program\ files\ (x86)\atmel\studio\7.0\toolchain\avr8\avr8-gnu-toolchain\avr\include\stdint.h \
+ c:\program\ files\ (x86)\atmel\studio\7.0\toolchain\avr8\avr8-gnu-toolchain\avr\include\util\delay_basic.h \
+ c:\program\ files\ (x86)\atmel\studio\7.0\toolchain\avr8\avr8-gnu-toolchain\avr\include\math.h
+
+.././std_type.h:
+
+.././bit_math.h:
+
+.././DIO_Interface.h:
+
+.././Timer_Private.h:
+
+.././Timer_Confige.h:
+
+c:\program\ files\ (x86)\atmel\studio\7.0\toolchain\avr8\avr8-gnu-toolchain\avr\include\avr\delay.h:
+
+c:\program\ files\ (x86)\atmel\studio\7.0\toolchain\avr8\avr8-gnu-toolchain\avr\include\util\delay.h:
+
+c:\program\ files\ (x86)\atmel\studio\7.0\toolchain\avr8\avr8-gnu-toolchain\avr\include\inttypes.h:
+
+c:\program\ files\ (x86)\atmel\studio\7.0\toolchain\avr8\avr8-gnu-toolchain\lib\gcc\avr\5.4.0\include\stdint.h:
+
+c:\program\ files\ (x86)\atmel\studio\7.0\toolchain\avr8\avr8-gnu-toolchain\avr\include\stdint.h:
+
+c:\program\ files\ (x86)\atmel\studio\7.0\toolchain\avr8\avr8-gnu-toolchain\avr\include\util\delay_basic.h:
+
+c:\program\ files\ (x86)\atmel\studio\7.0\toolchain\avr8\avr8-gnu-toolchain\avr\include\math.h:
---
+++ AVR_Timer/AVR_Timer/main.c
@@ -0,0 +1,33 @@
+/*
+ * AVR_Timer.c
+ *
+ * Created: 7/10/2021 11:14:05 PM
+ * Author : Mohamed
+ */ 
+
+#include "std_type.h"
+#include "DIO_Interface.h"
+#include "Timer_Interface.h"
+
+#define F_CPU 8000000
+
+
+int main(void)
+{
+	Timer0_Init();
+    DIO_VidSetPinDirection(DIO_PORTA,DIO_PIN0,DIO_OUTPUT);
+	
+	
+  
+    while (1) 
+    {
+		
+		
+		
+		Timer0_Delay(1000000);
+		
+		
+		
+    }
+}
+
