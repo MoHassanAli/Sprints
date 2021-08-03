34c55450ebcc0b61c29fae7a9ea0016b4ee6171d
MoHassanAli <86575412+MoHassanAli@users.noreply.github.com>
2021-08-03 23:59:44 +0200

    Add files via upload

---
+++ ATM_Project/ATM_Terminal/Debug/ATM_Terminal/HAL/MOTOR/MOTOR_Program.d
@@ -0,0 +1,18 @@
+ATM_Terminal/HAL/MOTOR/MOTOR_Program.o \
+  ATM_Terminal/HAL/MOTOR/MOTOR_Program.o:  \
+ ../ATM_Terminal/HAL/MOTOR/MOTOR_Program.c \
+  E:\Sprints\ATM_Project\ATM_Project\ATM_Terminal\ATM_Terminal\MCAL\DIO/DIO_Interface.h \
+  E:\Sprints\ATM_Project\ATM_Project\ATM_Terminal\ATM_Terminal\lib/DataTypes.h \
+  E:\Sprints\ATM_Project\ATM_Project\ATM_Terminal\ATM_Terminal\lib/BIT_Math.h \
+  E:\Sprints\ATM_Project\ATM_Project\ATM_Terminal\ATM_Terminal\lib/Memmap.h \
+  ../ATM_Terminal/HAL/MOTOR/MOTOR_Config.h
+
+E:\Sprints\ATM_Project\ATM_Project\ATM_Terminal\ATM_Terminal\MCAL\DIO/DIO_Interface.h:
+
+E:\Sprints\ATM_Project\ATM_Project\ATM_Terminal\ATM_Terminal\lib/DataTypes.h:
+
+E:\Sprints\ATM_Project\ATM_Project\ATM_Terminal\ATM_Terminal\lib/BIT_Math.h:
+
+E:\Sprints\ATM_Project\ATM_Project\ATM_Terminal\ATM_Terminal\lib/Memmap.h:
+
+../ATM_Terminal/HAL/MOTOR/MOTOR_Config.h:
---
+++ ATM_Project/ATM_Terminal/Debug/ATM_Terminal/SERVICE/SER_UART/SER_UART.d
@@ -0,0 +1,33 @@
+ATM_Terminal/SERVICE/SER_UART/SER_UART.o \
+  ATM_Terminal/SERVICE/SER_UART/SER_UART.o:  \
+ ../ATM_Terminal/SERVICE/SER_UART/SER_UART.c \
+  ../ATM_Terminal/SERVICE/SER_UART/SER_UART_int.h \
+  E:\Sprints\ATM_Project\ATM_Project\ATM_Terminal\ATM_Terminal\APP/../lib/DataTypes.h \
+  E:\Sprints\ATM_Project\ATM_Project\ATM_Terminal\ATM_Terminal\APP/../lib/BIT_Math.h \
+  E:\Sprints\ATM_Project\ATM_Project\ATM_Terminal\ATM_Terminal\APP/../MCAL/UART/UART_int.h \
+  E:\Sprints\ATM_Project\ATM_Project\ATM_Terminal\ATM_Terminal\APP/../MCAL/UART/../../lib/DataTypes.h \
+  E:\Sprints\ATM_Project\ATM_Project\ATM_Terminal\ATM_Terminal\APP/../MCAL/UART/../../lib/BIT_Math.h \
+  E:\Sprints\ATM_Project\ATM_Project\ATM_Terminal\ATM_Terminal\APP/../MCAL/UART/UART_config.h \
+  E:\Sprints\ATM_Project\ATM_Project\ATM_Terminal\ATM_Terminal\APP/../MCAL/UART/UART_private.h \
+  E:\Sprints\ATM_Project\ATM_Project\ATM_Terminal\ATM_Terminal\APP/../MCAL/UART/../../lib/Memmap.h \
+  E:\Sprints\ATM_Project\ATM_Project\ATM_Terminal\ATM_Terminal\APP/../lib/Memmap.h
+
+../ATM_Terminal/SERVICE/SER_UART/SER_UART_int.h:
+
+E:\Sprints\ATM_Project\ATM_Project\ATM_Terminal\ATM_Terminal\APP/../lib/DataTypes.h:
+
+E:\Sprints\ATM_Project\ATM_Project\ATM_Terminal\ATM_Terminal\APP/../lib/BIT_Math.h:
+
+E:\Sprints\ATM_Project\ATM_Project\ATM_Terminal\ATM_Terminal\APP/../MCAL/UART/UART_int.h:
+
+E:\Sprints\ATM_Project\ATM_Project\ATM_Terminal\ATM_Terminal\APP/../MCAL/UART/../../lib/DataTypes.h:
+
+E:\Sprints\ATM_Project\ATM_Project\ATM_Terminal\ATM_Terminal\APP/../MCAL/UART/../../lib/BIT_Math.h:
+
+E:\Sprints\ATM_Project\ATM_Project\ATM_Terminal\ATM_Terminal\APP/../MCAL/UART/UART_config.h:
+
+E:\Sprints\ATM_Project\ATM_Project\ATM_Terminal\ATM_Terminal\APP/../MCAL/UART/UART_private.h:
+
+E:\Sprints\ATM_Project\ATM_Project\ATM_Terminal\ATM_Terminal\APP/../MCAL/UART/../../lib/Memmap.h:
+
+E:\Sprints\ATM_Project\ATM_Project\ATM_Terminal\ATM_Terminal\APP/../lib/Memmap.h:
